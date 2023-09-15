4f928e0bfdf16ee7ab6824ee69602576242be841
inimicin <osmycin.amelia21@students.unila.ac.id>
2023-09-15 19:23:58 +0700

    add form input

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/profile/(:any)/(:any)/(:any)', 'Home::profile/$1/$2/$3');
+$routes->get('/user/profile', 'UserController::profile');
+$routes->get('/user/create', 'UserController::create');
+$routes->post('/user/store', 'UserController::store');
